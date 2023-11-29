c907830a3d85f19f4300a0cfc70c89f4aee4886b
Vincent Quivy <81567975+VincentQvy@users.noreply.github.com>
2023-11-29 14:14:31 +0100

    Tout bon

--- Exercice 0 - Familiarisez vous avec la bdd/familiarisation.js
+++ Exercice 0 - Familiarisez vous avec la bdd/familiarisation.js
@@ -1,11 +1,55 @@
 // 1 Quels sont les différents bases de données atteignable via cette connexion ? Quelle commande permet de les afficher?
 
+/*
+exercises           
+group_1             
+group_10            
+group_11            
+group_12            
+group_13            
+group_2             
+group_3             
+group_4             
+group_5             
+group_6             
+group_7             
+group_8             
+group_9            
+nomdegroupe         
+sample_airbnb       
+sample_analytics     
+sample_geospatial  
+sample_guides       
+sample_mflix        
+sample_restaurants    
+sample_supplies       
+sample_training      
+sample_weatherdata    
+test                 
+admin
+local
+
+show dbs
+
+*/
+
 // 2 Allez dans la BDD "exercices". Quelle commande permet de se déplacer à une nouvelle base de données. Que se passe-t-il si cette base n'existe pas ?
+
+// use exercises, ca te change de db
 
 // 3 Quelles sont les différentes collections présentes dans cette BDD ? Quelle commande permet de les afficher.
 
+// show collections 
+/*
+bel20
+cheeses
+irons
+laptops 
+*/
 
 // 4 Quelle est la commande nécessaire pour affichez la liste des documents d'une des collections.
+
+// db.collectionName.find().pretty()
 
 
 // Vous allez maintenant changer de BDD pour pouvoir travailler sur vos propres documents.
--- Exercice 1 - find, insert, update, remove/1. laptops.js
+++ Exercice 1 - find, insert, update, remove/1. laptops.js
@@ -1,19 +1,66 @@
 // 1 Donnez liste des ordinateurs portables avec un prix compris entre 500 et 1000 euros ou avec un disque dur de 750 Go.
 
+/*
+
+[
+  {
+    _id: ObjectId('65670cdebb2341fb8bcaabbb'),
+    name: 'Acer Aspire F5-573G-57GD',
+    price: 749,
+    properties: {
+      processor: 'Intel Core i5',
+      RAM: '8GB',
+      HD: '650GB',
+      OS: 'Windows 10',
+      USB: { USB2_0: 0, USB3_0: 2, USB3_1: 1 }
+    },
+    LanguageOS: [ 'Dutch', 'French', 'English' ],
+    reviews: [
+      { by: 'Aline', score: 4, text: 'Very well' },
+      { by: 'Me', score: 3.5, text: 'Compact' }
+    ]
+  },
+  {
+    _id: ObjectId('65670cdebb2341fb8bcaabbc'),
+    name: 'HP Pavilion 17-g101nd',
+    price: 534,
+    properties: {
+      processor: 'Intel Pentium N3700',
+      RAM: '4GB',
+      HD: '1000GB',
+      HDSpeed: 5400,
+      OS: 'Windows 10',
+      displayType: 'Shiny',
+      USB: { USB2_0: 0, USB3_0: 3 }
+    },
+    LanguageOS: [ 'Dutch', 'French', 'English', 'German' ],
+    networkConnections: [ 'bluetooth', 'wi-fi', 'ethernet' ],
+    reviews: [
+      { by: 'Melanie', score: 4.3, text: 'Very good' },
+      { by: 'Eric', score: 4.5, text: 'Nice' }
+    ]
+  }
+]
+
+db.laptops.find(( { $or: [ {'properties.HD': {$eq: 750}},{'price': { $gt: 500 , $lt: 1000}} ]}))
+*/
 
 // 2 Changez Windows 10 vers Windows 8
 
+// db.laptops.updateMany({'properties.OS': 'Windows 10'}, {$set: { 'properties.OS': 'Windows 8' }})
 
 // 3 Donnez le nom, le prix et le système d'exploitation des 2 ordinateurs portables les moins chers.
 
+//db.laptops.find({}, { name: 1, price: 1, "properties.OS": 1 }).sort({ price: 1 }).limit(2)
 
 // 4 Donnez à tous les ordinateurs portables avec au moins 1 avis avec une note comprise entre 4 et 4,2
 
+//db.laptops.find({"reviews": {$elemMatch: {"score": {$gte: 4,$lte: 4.2}}}})
 
 // Donnez à tous les ordinateurs portables dotés de 2 ports USB 3_0 et prenant en charge au moins 2 langues, dont le "Dutch"
 
-
-
+//db.laptops.find({"properties.USB.USB3_0": 2,$expr: {$and: [{ $in: ["Dutch", "$LanguageOS"] },{ $gte: [{ $size: "$LanguageOS" }, 2] }]}})
+  
 
 
 
